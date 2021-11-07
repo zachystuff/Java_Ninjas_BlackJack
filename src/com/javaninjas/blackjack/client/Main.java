de7c11832132517e58b1294d4da2166a56a2de58
Alan-SDE <apotsy8251@gmail.com>
2021-11-07 17:33:43 -0500

    Added banner1.txt and gameover.txt Added Introduction class and logic finished BlackJackApp logic

--- src/com/javaninjas/blackjack/client/Main.java
+++ src/com/javaninjas/blackjack/client/Main.java
@@ -2,9 +2,11 @@ package com.javaninjas.blackjack.client;
 
 import com.javaninjas.blackjack.controller.BlackJackApp;
 
+import java.io.IOException;
+
 class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BlackJackApp app = new BlackJackApp();
         app.execute();
     }
