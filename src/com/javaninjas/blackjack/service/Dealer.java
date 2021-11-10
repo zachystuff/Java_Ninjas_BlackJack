e3199751ab8a8abe896548b770b1cc8f59e4be30
Subash KC <subash.kc@tlgcohort.com>
2021-11-10 10:26:58 -0800

    Added zach name in dealer class

--- src/com/javaninjas/blackjack/service/Dealer.java
+++ src/com/javaninjas/blackjack/service/Dealer.java
@@ -20,8 +20,8 @@ import java.util.stream.Stream;
 
 /**
  * Dealer class for BlackJack game and extends Player class. It consists of Player List which is public and static
- * And deck of Cards
- * @author Subash KC
+  * And deck of Cards
+ * @author Subash KC, Abdulrazak Yusuf
  * version 1.0
  */
 public class Dealer extends Player {
